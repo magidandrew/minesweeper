e29b51d1fce6d35f1ecc8bbdffacdb7591175f28
Andrew Magid <magid.andrew@gmail.com>
2021-12-24 14:44:36 -0600

    count bug fix

--- main.py
+++ main.py
@@ -3,40 +3,52 @@ import config_globals as gb
 from random import random
 from pathlib import Path
 
-def print_mat(mat: list[list[int]]):
-    for i in mat:
-        print(i)
 
-def create_mine_mat(x: int, y: int, mine_prob: float = .2) -> list[list[int]]:
+def print_mat(mat: list[list[int]]):
+    for row in mat:
+        print(row)
+
+
+def create_mine_mat(x: int, y: int, mine_num: int = 20, mine_prob: float = .2) -> list[list[int]]:
+    if mine_num > x*y:
+        raise RuntimeError("Too many mines for board!")
     mines = [[0] * y for _ in range(x)]
-    for row in range(x):
-        for col in range(y):
-            if random() <= mine_prob:
-                mines[row][col] = 1
+    count = 0
+    while count < mine_num:
+        for row in range(x):
+            for col in range(y):
+                if count == mine_num:
+                    break
+                if random() <= mine_prob and mines[row][col] == 0:
+                    mines[row][col] = 1
+                    count += 1
     return mines
 
+
 def create_hint_mat(mine_mat: list[list[int]]) -> list[list[int]]:
-    def isbomb_at(x: int, y:int) -> int:
+    def isbomb_at(x: int, y: int) -> int:
         if x < 0 or y < 0:
             return 0
         try:
             return 1 if mine_mat[x][y] != 0 else 0
         except IndexError:
             return 0
+
     rows = len(mine_mat)
     cols = len(mine_mat[0])
     hints = [[0] * cols for _ in range(rows)]
     for x in range(rows):
         for y in range(cols):
-            hints[x][y] += isbomb_at(x-1, y-1)  # Top Left
-            hints[x][y] += isbomb_at(x, y-1)    # Top Middle
-            hints[x][y] += isbomb_at(x+1, y-1)  # Top Right
-            hints[x][y] += isbomb_at(x-1, y)    # Left
-            hints[x][y] += isbomb_at(x+1, y)    # Right
-            hints[x][y] += isbomb_at(x-1, y+1)  # Lower Left
-            hints[x][y] += isbomb_at(x, y+1)    # Lower Middle
-            hints[x][y] += isbomb_at(x+1, y+1)  # Lower Right
+            hints[x][y] += isbomb_at(x - 1, y - 1)  # Top Left
+            hints[x][y] += isbomb_at(x, y - 1)  # Top Middle
+            hints[x][y] += isbomb_at(x + 1, y - 1)  # Top Right
+            hints[x][y] += isbomb_at(x - 1, y)  # Left
+            hints[x][y] += isbomb_at(x + 1, y)  # Right
+            hints[x][y] += isbomb_at(x - 1, y + 1)  # Lower Left
+            hints[x][y] += isbomb_at(x, y + 1)  # Lower Middle
+            hints[x][y] += isbomb_at(x + 1, y + 1)  # Lower Right
     return hints
+
 
 def main():
     pg.init()
@@ -55,7 +67,7 @@ def main():
     y_offset_bottom = int(gb.SCREENY * 0.03)
     y_offset_score = int(1.5 * y_offset_bottom)
     # draw playing area
-    pg.draw.rect(screen, gb.DARK_GREY, (x_offset, y_offset_top , gb.SCREENX - 2 * x_offset,
+    pg.draw.rect(screen, gb.DARK_GREY, (x_offset, y_offset_top, gb.SCREENX - 2 * x_offset,
                                         gb.SCREENY - y_offset_top - y_offset_bottom), gb.LINE_WIDTH, 1)
 
     # draw score area
@@ -74,7 +86,7 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    mat = create_mine_mat(8,8)
+    mat = create_mine_mat(8, 8, mine_num=10)
     print_mat(mat)
     print("hints")
     print_mat(create_hint_mat(mat))
