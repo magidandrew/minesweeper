0c3013ff9fb0d10e331daee7b47473c8542e9b80
Andrew Magid <magid.andrew@gmail.com>
2021-12-24 14:36:12 -0600

    Revert " optimized hint generation O(n) not O(2n)"
    This reverts commit cbd4d9b61c5753bc10024b1046199baf2538a1e0.

--- main.py
+++ main.py
@@ -3,82 +3,40 @@ import config_globals as gb
 from random import random
 from pathlib import Path
 
-
 def print_mat(mat: list[list[int]]):
     for i in mat:
         print(i)
 
-
-def create_field(x: int, y: int, mine_num: int = 20, mine_prob: float = .2) -> tuple[
-        list[list[int]], list[list[int]]]:
+def create_mine_mat(x: int, y: int, mine_prob: float = .2) -> list[list[int]]:
     mines = [[0] * y for _ in range(x)]
-    hints = [[0] * y for _ in range(x)]
-    count = 0
-
-    # pass in bomb location and try to
-    def inc_hint(bomb_x: int, bomb_y: int):
-
-        def try_add(hint_x: int, hint_y: int):
-            try:
-                # prevent negative indexing
-                if bomb_x < 0 or bomb_y < 0:
-                    return
-                # check that bomb doesn't exist at that location
-                if mines[hint_x][hint_y] == 1:
-                    return
-                hints[hint_x][hint_y] += 1
-            except IndexError:
-                pass
-
-        try_add(bomb_x - 1, bomb_y - 1)  # Top Left
-        try_add(bomb_x, bomb_y - 1)  # Top Middle
-        try_add(bomb_x + 1, bomb_y - 1)  # Top Right
-        try_add(bomb_x - 1, bomb_y)  # Left
-        try_add(bomb_x + 1, bomb_y)  # Right
-        try_add(bomb_x - 1, bomb_y + 1)  # Lower Left
-        try_add(bomb_x, bomb_y + 1)  # Lower Middle
-        try_add(bomb_x + 1, bomb_y + 1)  # Lower Right
-
-    def gen(count, mines):
-        for row in range(x):
-            for col in range(y):
-                if count == mine_num:
-                    break
-                if random() <= mine_prob and mines[row][col] == 0:
-                    # mine added
-                    mines[row][col] = 1
-                    inc_hint(row, col)
-                    count += 1
-
-    if count < mine_num:
-        gen(count, mines)
-    return mines, hints
-
+    for row in range(x):
+        for col in range(y):
+            if random() <= mine_prob:
+                mines[row][col] = 1
+    return mines
 
 def create_hint_mat(mine_mat: list[list[int]]) -> list[list[int]]:
-    def isbomb_at(x: int, y: int) -> int:
+    def isbomb_at(x: int, y:int) -> int:
         if x < 0 or y < 0:
             return 0
         try:
             return 1 if mine_mat[x][y] != 0 else 0
         except IndexError:
             return 0
-
     rows = len(mine_mat)
     cols = len(mine_mat[0])
     hints = [[0] * cols for _ in range(rows)]
     for x in range(rows):
         for y in range(cols):
-            hints[x][y] += isbomb_at(x - 1, y - 1)  # Top Left
-            hints[x][y] += isbomb_at(x, y - 1)  # Top Middle
-            hints[x][y] += isbomb_at(x + 1, y - 1)  # Top Right
-            hints[x][y] += isbomb_at(x - 1, y)  # Left
-            hints[x][y] += isbomb_at(x + 1, y)  # Right
-            hints[x][y] += isbomb_at(x - 1, y + 1)  # Lower Left
-            hints[x][y] += isbomb_at(x, y + 1)  # Lower Middle
-            hints[x][y] += isbomb_at(x + 1, y + 1)  # Lower Right
+            hints[x][y] += isbomb_at(x-1, y-1)  # Top Left
+            hints[x][y] += isbomb_at(x, y-1)    # Top Middle
+            hints[x][y] += isbomb_at(x+1, y-1)  # Top Right
+            hints[x][y] += isbomb_at(x-1, y)    # Left
+            hints[x][y] += isbomb_at(x+1, y)    # Right
+            hints[x][y] += isbomb_at(x-1, y+1)  # Lower Left
+            hints[x][y] += isbomb_at(x, y+1)    # Lower Middle
+            hints[x][y] += isbomb_at(x+1, y+1)  # Lower Right
     return hints
-
 
 def main():
     pg.init()
@@ -97,7 +55,7 @@ def main():
     y_offset_bottom = int(gb.SCREENY * 0.03)
     y_offset_score = int(1.5 * y_offset_bottom)
     # draw playing area
-    pg.draw.rect(screen, gb.DARK_GREY, (x_offset, y_offset_top, gb.SCREENX - 2 * x_offset,
+    pg.draw.rect(screen, gb.DARK_GREY, (x_offset, y_offset_top , gb.SCREENX - 2 * x_offset,
                                         gb.SCREENY - y_offset_top - y_offset_bottom), gb.LINE_WIDTH, 1)
 
     # draw score area
@@ -116,8 +74,7 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    mat = create_field(8, 8, mine_num=10)
-    print_mat(mat[0])
+    mat = create_mine_mat(8,8)
+    print_mat(mat)
     print("hints")
-    print_mat(mat[1])
-    # print_mat(create_hint_mat(mat))
+    print_mat(create_hint_mat(mat))
